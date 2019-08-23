98f812b31ef7bb4364041a3aa5b5fe5b48dc02a7
Carlos <miklo.concepcion@gmail.com>
2019-08-22 23:43:16 -0500

    completing stretch tasks. in the morning will work on creating new components and passing props to the index. re-read tk

--- react-american-football/src/App.js
+++ react-american-football/src/App.js
@@ -14,12 +14,12 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   // Declare a new state variable
-//   const [reset, updateScore] = useState(0);
+  //   const [reset, updateScore] = useState(0);
   const [value, setInitialValue] = useState(0);
-  const [scoreLions, updateLions] = useState(32);
-  const [scoreTigers, updateTigers] = useState(32);
-//   most common 
-// const [state, setState] = useState()
+  const [scoreLions, setLions] = useState(32);
+  const [scoreTigers, setTigers] = useState(32);
+  //   most common 
+  // const [state, setState] = useState()
 
   return (
     <div className="container">
@@ -40,24 +40,25 @@ function App() {
         </div>
         <BottomRow />
       </section>
+
       <section className="buttons">
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           {/* <button className="homeButtons__touchdown" onClick={() => updateScore(value + 7)}>Home Touchdown</button> */}
           {/* <button className="homeButtons__fieldGoal" onClick={() => updateScore(value + 3)}>Home Field Goal</button> */}
-          <button className="homeButtons__touchdown" onClick={() => updateLions(scoreLions + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => updateLions(scoreLions + 3)}>Home Field Goal</button>
-          <button onClick={() => updateLions(32)}>Reset</button>
+          <button className="homeButtons__touchdown" onClick={() => setLions(scoreLions + 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setLions(scoreLions + 3)}>Home Field Goal</button>
+          <button onClick={() => setLions(32)}>Reset</button>
           <button onClick={() => setInitialValue(0)}>Reset/zero</button>
 
         </div>
         <div className="awayButtons">
           {/* <button className="awayButtons__touchdown" onClick={() => updateScore(value + 7)}>Away Touchdown</button> */}
           {/* <button className="awayButtons__fieldGoal" onClick={() => updateScore(value + 3)}>Away Field Goal</button> */}
-          <button className="awayButtons__touchdown" onClick={() => updateTigers(scoreTigers + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => updateTigers(scoreTigers + 3)}>Away Field Goal</button>
-          <button onClick={() => updateTigers(32)}>Reset</button>          
+          <button className="awayButtons__touchdown" onClick={() => setTigers(scoreTigers + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setTigers(scoreTigers + 3)}>Away Field Goal</button>
+          <button onClick={() => setTigers(32)}>Reset</button>
           <button onClick={() => setInitialValue(0)}>Reset/zero</button>
         </div>
       </section>
---
+++ react-american-football/src/TopRow.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import "./App.css";
+
+
+// const TopRow = () => {
+//     return (
+//         <div className="topRow">
+//             <div className="home">
+//                 <h2 className="home__name">Lions</h2>
+
+//                 {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+
+//                 <div className="home__score">{scoreLions}</div>
+//             </div>
+//             <div className="timer">00:03</div>
+//             <div className="away">
+//                 <h2 className="away__name">Tigers</h2>
+//                 <div className="away__score">{scoreTigers}</div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default TopRow;
